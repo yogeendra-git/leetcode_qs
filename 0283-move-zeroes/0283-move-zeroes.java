4fd7d1de19e3eae765d060fd5e0f367241363d34
yogeendra-git <mr.yogeendra@gmail.com>
2026-08-24 16:21:06 +0530

    Time: 2 ms (91.96%), Space: 47.7 MB (76.48%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -0,0 +1,14 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int zeros=0;
+        for (int i=0;i<nums.length;i++){
+            if (nums[i]!=0){
+                nums[zeros]=nums[i];
+                zeros++;
+            }
+        }
+        for (int i=zeros;i<nums.length;i++){
+            nums[i]=0;
+        }
+    }
+}
