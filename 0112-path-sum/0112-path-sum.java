074b53fd400b7407ddd78acae2f6a286c6dd1837
Yogeendra G <mr.yogeendra@gmail.com>
2026-09-02 22:10:21 +0530

    Time: 0 ms (100%), Space: 44.9 MB (67.2%) - LeetHub

--- 0112-path-sum/0112-path-sum.java
+++ 0112-path-sum/0112-path-sum.java
@@ -16,9 +16,18 @@
 class Solution {
     public boolean hasPathSum(TreeNode root, int targetSum) {
         if(root==null) return false;
+        return dfs(root,targetSum);
+
+        
+    }
+    private boolean dfs(TreeNode root,int targetSum){
+        if(root==null) return false;
+        targetSum-=root.val;
         if(root.left==null && root.right==null){
-            return targetSum==root.val;
-        }
-        return hasPathSum(root.left,targetSum-root.val) || hasPathSum(root.right,targetSum-root.val);
+            return targetSum==0;
+        }return hasPathSum(root.left,targetSum) || hasPathSum(root.right,targetSum);
     }
 }
+
+        
+        
